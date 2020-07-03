f204542bf49edadebddcee880eb8f2d13ef89011
60102 <leechenglok@hotmail.com>
2020-07-03 19:42:49 +0800

    commit by cheng lok

--- app/src/main/java/com/example/practical_2_part_a/MainActivity.java
+++ app/src/main/java/com/example/practical_2_part_a/MainActivity.java
@@ -9,41 +9,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private int mCount=0;
+    private int mCount = 0;
     private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mShowCount=(TextView) findViewById(R.id.show_count);
-    }
 
-    public void showToast(View view){
-        Toast toast=Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
-
-        toast.show();
     }
-    public void countUp(View view){
-        ++mCount;
-        if(mShowCount!=null){
-            mShowCount.setText(Integer.toString(mCount));
-        }
-        if(mCount>0){
-            findViewById(R.id.button_zero).setBackgroundColor(Color.BLACK);
-        }
-        if(mCount%2==0){
-            view.setBackgroundColor(Color.BLUE);
-        }
-        else
-            view.setBackgroundColor(Color.GREEN);
-    }
-    public void zero(View view){
-        mCount=0;
-        mShowCount.setText(Integer.toString(0));
-        if(mCount==0){
-            view.setBackgroundColor(Color.GRAY);
-        }
-    }
-
 }
